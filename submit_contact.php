e5a43cc7419b34fe1948787d091039bd48a6cf8d
Ismahene <ismahene12345@gmail.com>
2026-09-11 09:21:01 +0200

    securisez votre code

--- submit_contact.php
+++ submit_contact.php
@@ -29,7 +29,7 @@ return;
                 <div class="card-body">
                     <h5 class="card-title">Rappel de vos informations</h5>
                     <p class="card-text"><b>Email</b> : <?php echo $_POST['email']; ?></p>
-                    <p class="card-text"><b>Message</b> : <?php echo $_POST['message']; ?></p>
+                    <p class="card-text"><b>Message</b> : <?php echo strip_tags($_POST['message']); ?></p>
                 </div>
             </div>
         <?php
