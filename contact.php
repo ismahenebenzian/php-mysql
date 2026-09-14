6901f0ad9bb50c06997167a6964cb70792219a65
Ismahene <ismahene12345@gmail.com>
2026-09-14 13:44:55 +0200

    Paramétrez le formulaire d'envoi de fichier

--- contact.php
+++ contact.php
@@ -19,8 +19,7 @@
         <h1>Contactez nous</h1>
 
         <!-- Formulaire en GET  -->
-        <form action="submit_contact.php" method="POST">
-
+        <form action="submit_contact.php" method="POST" nctype="multipart/form-data">
             <div class="mb-3">
                 <label for="email" class="form-label">Email</label>
                 <input type="email" class="form-control" id="email" name="email" aria-describedby="email-help">
@@ -31,7 +30,15 @@
                 <label for="message" class="form-label">Votre message</label>
                 <textarea class="form-control" placeholder="Exprimez vous" id="message" name="message"></textarea>
             </div>
+            
+            <!-- Ajout champ d'upload ! -->
+            <div class="mb-3">
+                <label for="screenshot" class="form-label">Votre capture d'écran</label>
+                <input type="file" class="form-control" id="screenshot" name="screenshot" />
+            </div>
+
             <button type="submit" class="btn btn-primary">Envoyer</button>
+
         </form>
 
         <br />
